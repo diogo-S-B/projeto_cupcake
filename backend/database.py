fe28dd03c01a6e43a9fbb64d61ba3e3af45769a1
Diogo S. B <114962803+diogo-S-B@users.noreply.github.com>
2025-11-07 14:35:33 -0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 MYSQL_URL = (
-    "mysql+mysqlconnector://avnadmin:AVNS_MHbvjkqsU7rk8B_PWMG"
-    "@cupcake-diguroos-1a48.e.aivencloud.com:15718/defaultdb?ssl-mode=REQUIRED"
+    "mysql+pymysql://avnadmin:AVNS_MHbvjkqsU7rk8B_PWMG"
+    "@cupcake-diguroos-1a48.e.aivencloud.com:15718/defaultdb"
 )
+
 
 engine = create_engine(MYSQL_URL, connect_args={"ssl_disabled": True})
 
